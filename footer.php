c9c51fd826c677b4b1acf4c64d60f113153d0921
mrfakename <me@mrfake.name>
2022-10-14 18:39:21 -0700

    Changed phpmag attribution

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </main>
 <div class="footer">
     <h2><?=$name?></h2>
-    <p>&copy; 2022 <?=$name?>. All rights reserved. Powered by phpmag.</p>
+    <p>&copy; 2022 <?=$name?>. All rights reserved. Powered by <a href="https://github.com/phpmag/phpmag" target="_blank">phpmag</a>.</p>
 </div>
 </body>
 </html>
